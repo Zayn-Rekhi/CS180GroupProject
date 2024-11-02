3a98dc1ee6555cbcb8e464e794a646ed8720a05c
Sri Madur <sri.madur@gmail.com>
2024-11-02 02:38:01 -0400

    created test case for the Post class

--- src/PostInterface.java
+++ src/PostInterface.java
@@ -1,6 +1,9 @@
 package src;
+import java.util.ArrayList;
 
 public interface PostInterface {
+    //accessor methods
+    ArrayList<Comment> getComments();
     User getUser();
     int getPostID();
     String getTitle();
@@ -10,16 +13,22 @@ public interface PostInterface {
     int getLikes();
     int getDislikes();
     
+    //mutator methods
     void setUser(User user);
     void setTitle(String title);
     void setImageURL(String imageURL);
     void setDescription(String description);
+
+    //managing likes and dislikes
     void addLike();
     void removeLike();
     void addDislike();
     void removeDislike();
+
+    //managing comments
     void addComment(Comment comment);
     boolean removeComment(Comment comment);
-    
+
+    //display post method
     String displayedPost();
 }
