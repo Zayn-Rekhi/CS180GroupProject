040b6271c40270d2ca2fedd71bcd11ba10d8ede1
NotNathanLee <lee5007@purdue.edu>
2024-11-17 16:58:42 -0500

    javadoc for DataTransferInterface.java

--- src/DataTransferInterface.java
+++ src/DataTransferInterface.java
@@ -1,3 +1,17 @@
+/**
+ * Data transfer Interface
+ * contains all the methods to be implemented in the Data transfer class
+ *
+ * @author zaynrekhi
+ * @author melody
+ * @author srimadur
+ * @author braydenbrafford
+ * @author nothanlee
+ * @version 1.0.0
+ *
+ * @version 1.0.0
+ */
+
 public interface DataTransferInterface {
     String getMessage();
     Object getValue();
