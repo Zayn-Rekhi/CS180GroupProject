57cbb1c5415f8eb5785f836a458a6ce87fd02741
Sri Madur <98134620+srimadur@users.noreply.github.com>
2024-11-17 16:54:43 -0500

    Update ClientInterface.java

--- src/ClientInterface.java
+++ src/ClientInterface.java
@@ -1,6 +1,17 @@
-/*
-java doc
-*/
+/**
+ * Client Interface
+ * contains all the methods to be implemented in the Client class
+ *
+ * @author zaynrekhi
+ * @author melody
+ * @author srimadur
+ * @author braydenbrafford
+ * @author nothanlee
+ * @version 1.0.0
+ *
+ * @version 1.0.0
+ */
+
 public interface ClientInterface {
     DataTransfer awaitResponse();
     boolean writeObject(DataTransfer data);
