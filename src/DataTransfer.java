bf8749a797cd9f81f2d2b55da040fa2032b7f3b0
NotNathanLee <lee5007@purdue.edu>
2024-11-17 17:09:17 -0500

    javadoc DataTransfer.java

--- src/DataTransfer.java
+++ src/DataTransfer.java
@@ -1,5 +1,19 @@
 import java.io.Serializable;
 
+/**
+ * Data Transfer Class
+ * This class handles a message and a value to manage data transfer between different parts of the social media app. 
+ * It supports serialization for storing or sending data and contains getter methods to access the message and value.
+ *
+ * @author zaynrekhi
+ * @author melody
+ * @author srimadur
+ * @author braydenbrafford
+ * @author nothanlee
+ * @version 1.0.0
+ *
+ */
+
 public class DataTransfer implements Serializable, DataTransferInterface {
     private String message;
     private Object value;
