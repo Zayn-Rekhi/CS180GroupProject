3df76537194dcee559ad0ac95ced7c69a38ba10a
NotNathanLee <lee5007@purdue.edu>
2024-11-17 17:10:17 -0500

    javadoc DataTransferTestsInterface.java

--- src/DataTransferTestsInterface.java
+++ src/DataTransferTestsInterface.java
@@ -1,3 +1,17 @@
+/**
+ * Data transfer tests Interface
+ * contains all the methods to be implemented in the Data transfer tests class
+ *
+ * @author zaynrekhi
+ * @author melody
+ * @author srimadur
+ * @author braydenbrafford
+ * @author nothanlee
+ * @version 1.0.0
+ *
+ * @version 1.0.0
+ */
+
 public interface DataTransferTestsInterface {
     void testDataTransferMessage();
     void testDataValue();
