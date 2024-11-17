70fac585530e61c7cdf18166255ee6c3638a93c7
B-BRAFF <bbraffor@purdue.edu>
2024-11-17 16:39:46 -0500

    Update ServerInterface.java

--- src/ServerInterface.java
+++ src/ServerInterface.java
@@ -1,3 +1,16 @@
+/**
+ * Server Interface
+ * contains all the methods to be implemented in the Server class
+ *
+ * @author zaynrekhi
+ * @author melody
+ * @author srimadur
+ * @author braydenbrafford
+ * @author nothanlee
+ * @version 1.0.0
+ *
+ * @version 1.0.0
+ */
 public interface ServerInterface {
     DataTransfer processUser(String command, DataTransfer data);
     DataTransfer processPost(String command, DataTransfer data);
